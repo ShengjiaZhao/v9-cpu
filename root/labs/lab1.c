a524b9166754234ea3630a1df0fe68728998921d
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-04-16 06:31:55 +0800

    Finished lab1

--- root/labs/lab1.c
+++ root/labs/lab1.c
@@ -1,56 +1,96 @@
 //
-// Created by shengjia on 4/15/16.
+// Created by shengjia based on v9-CPU on 4/15/16.
 //
 
 #include <u.h>
 #include <lab.h>
+#include <lab_ui.h>
 
-cout(char c)
+uint ticks = 0;
+char *mem_top;           // current top of unused memory
+uint mem_sz;             // size of physical memory
+
+void panic(char *s)
 {
-  out(1, c);
+  asm(CLI);
+  out(1,'p'); out(1,'a'); out(1,'n'); out(1,'i'); out(1,'c'); out(1,':'); out(1,' ');
+  while (*s) out(1,*s++);
+  out(1,'\n');
+  asm(HALT);
 }
 
-int consolewrite(struct inode *ip, char *buf, int n)
-{
-  int i, e;
-  for (i = 0; i < n; i++) cout(buf[i]);
-  return n;
+
+void trap(uint *sp, double g, double f, int c, int b, int a, int fc, uint *pc) {
+  uint va;
+  switch (fc) {
+    case FSYS: panic("FSYS from kernel");
+    case FSYS + USER:
+      printf("Haven't implemented syscall yet");
+      return;
+    case FTIMER:
+    case FTIMER + USER:
+      ticks++;
+      if (ticks % 1000 == 0)
+        printf("Tick %d\n", ticks);
+      if (ticks >= 10000) {
+        printf("Exiting successfully");
+        asm(HALT);
+      }
+      return;
+    default:
+      printf("Unknown interrupt");
+      asm(HALT);
+  }
 }
 
-void consoleinit()
+void alltraps()
 {
-  devsw[CONSOLE].write = consolewrite;
-  devsw[CONSOLE].read  = consoleread;
+  asm(PSHA);
+  asm(PSHB);
+  asm(PSHC);
+  asm(PSHF);
+  asm(PSHG);
+  asm(LUSP); asm(PSHA);
+  trap();                // registers passed back out by magic reference :^O
+  asm(POPA); asm(SUSP);
+  asm(POPG);
+  asm(POPF);
+  asm(POPC);
+  asm(POPB);
+  asm(POPA);
+  asm(RTI);
 }
 
-mainc()
+void mainc()
 {
-  consoleinit();         // console device
   ivec(alltraps);        // trap vector
-  stmr(128*1024);        // set timer
+  splx(1);               // enable interrupt
+  stmr(1024*128);        // set timer
   while(1);
 }
 
-main()
+void main()
 {
   int *ksp;                 // temp kernel stack pointer
   static char kstack[256];  // temp kernel stack
   static int endbss;        // last variable in bss segment
-
-  // initialize memory allocation
-  mem_top = kreserved = ((uint)&endbss + PAGE + 3) & -PAGE;
-  mem_sz = msiz();
-
-  // initialize kernel page table
-  setupkvm();
-  kpdir[0] = kpdir[(uint)USERTOP >> 22]; // need a 1:1 map of low physical memory for awhile
 
   // initialize kernel stack pointer
   ksp = ((uint)kstack + sizeof(kstack) - 8) & -8;
   asm(LL, 4);
   asm(SSP);
 
-  // jump (via return) to high memory
+  // This is some debugging code to print the value of SP
+  // asm(LEA, 0);
+  // asm(SL, 4);
+  // printf("%x\n", val);
+
+  // Important! this value must be reset because after SSP the stack has been moved
+  // and all contents of the original stack are lost
+  ksp = ((uint)kstack + sizeof(kstack) - 8) & -8;
   *ksp = (uint)mainc;
   asm(LEV);
+
+  printf("Should never get here!\n");
+  asm(HALT);
 }
---
+++ root/labs/run1.sh
@@ -0,0 +1,2 @@
+./xc -v -o bin/lab1 -I../lib lab1.c
+./xem bin/lab1
