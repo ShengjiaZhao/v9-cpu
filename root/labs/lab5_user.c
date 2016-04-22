eef4c0beab6595151c407771651525bd315b0824
s0271 <zhaosj12@mails.tsinghua.edu.cn>
2016-04-22 10:35:50 +0800

    User program for lab5

---
+++ root/labs/lab5_user.c
@@ -0,0 +1,10 @@
+#include <u.h>
+#include <libc.h>
+//#include <lab_stdout.h>
+
+int main(int argc, char *argv[]) {
+  //cout('c');
+  printf("Hello world, this is a user program\n");
+
+  return 0;
+}
